f53d3c4eabecd5f08b65903fb23df178cf1f73a0
Chris Badahdah <a.s.badahdah@gmail.com>
2019-08-07 14:09:42 -0700

    Adds storybook types for downstream support

--- @storybook/react.tsx
+++ @storybook/react.tsx
@@ -1,7 +1,7 @@
 import { storiesOf as of } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
 
-export const storiesOf = (name, m) => {
+export const storiesOf = (name: string, m: NodeModule) => {
   const stories = of(name, m)
   stories.addDecorator(withInfo)
   return stories
