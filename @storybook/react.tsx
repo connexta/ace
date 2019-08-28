a55b301ef46080837af74da87e279db62c09467d
Blake Thomson <blake.thomson@connexta.com>
2019-08-28 12:26:58 -0700

    Add hook support to stories (#42)

--- @storybook/react.tsx
+++ @storybook/react.tsx
@@ -4,5 +4,9 @@ import { withInfo } from '@storybook/addon-info'
 export const storiesOf = (name: string, m: NodeModule) => {
   const stories = of(name, m)
   stories.addDecorator(withInfo)
+
+  // Hack to make hooks work with storybook. Real fix available in https://github.com/storybookjs/storybook/releases/tag/v5.2.0-beta.10
+  stories.addDecorator((Story: any) => <Story />)
+
   return stories
 }
