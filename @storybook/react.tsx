20753c98a3540627ec6dd47568da94706bd1f5a8
andrewkfiedler <andrewkfiedler@gmail.com>
2019-10-14 15:57:46 -0700

    wip

--- @storybook/react.tsx
+++ @storybook/react.tsx
@@ -4,7 +4,7 @@ import * as React from 'react'
 
 export const storiesOf = (name: string, m: NodeModule) => {
   const stories = of(name, m)
-  stories.addDecorator(withInfo)
+  stories.addDecorator(withInfo as any)
 
   // Hack to make hooks work with storybook. Real fix available in https://github.com/storybookjs/storybook/releases/tag/v5.2.0-beta.10
   stories.addDecorator((Story: any) => <Story />)
